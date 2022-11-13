c54dfb7a57abdfbf4cb7aaacb4caad59d777c5fa
Michael Wekesa <wekesa360@outlook.com>
2022-11-13 14:52:55 +0300

    update: add all models

--- ems_event/models.py
+++ ems_event/models.py
@@ -1,3 +1,125 @@
+from django.urls import reverse
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from autoslug import AutoSlugField
 
-# Create your models here.
+
+class UserProfile(AbstractUser):
+    TYPE_CHOICES = (
+        ('lecturer', 'lecturer'),
+        ('student', 'student')
+    )
+    username = None
+    email = models.EmailField(('email address'), unique=True)
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    user_type = models.CharField(choices=TYPE_CHOICES)
+    avatar = models.FileField(upload_to='user/uploads/avatar/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    upadated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self) -> str:
+        return self.first_name
+
+    def get_avatar_url(self) -> str:
+        return self.avatar.url
+
+
+class Category(models.Model):
+    CATEGORY_CHOICES = (
+        ('university', 'university'),
+        ('school', 'school'),
+        ('association', 'association'),
+        ('class', 'class'),
+        ('club', 'club')
+    )
+    category = models.CharField(max_length=80, choices=CATEGORY_CHOICES)
+    type = models.CharField(max_length=256) # e.g roadtrip, cultural show, gala, webinar etc
+    slug = AutoSlugField(populate_from='category', unique_with='id')
+    created_at = models.DateTimeField(auto_add_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self) -> str:
+        return self.category
+    
+    def get_absolute_url(self) -> str:
+        return reverse("event:category", kwargs={"slug": self.slug})
+    
+    class Meta:
+        db_table = 'categories'
+    
+
+class Event(models.Model):
+    TYPE_CHOICES = (
+        ('online', 'online'),
+        ('hybrid', 'hybrid'),
+        ('physical', 'physical')
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    event_owner = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    event_title = models.TextField()
+    description = models.TextField()
+    event_type = models.CharField(max_length=80, choices=TYPE_CHOICES)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    number_attendees = models.PositiveIntegerField(blank=True)
+    venue = models.CharField(max_length=256) # if online virtual link
+    status = models.CharField(max_length=80, default='upcoming')
+    slug = AutoSlugField(populate_from='event_title', unique_with='id')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def ___str__(self)-> str:
+        return self.event_title
+    
+    def get_absolute_url(self):
+        return reverse('ems_event:event', kwargs={'slug': self.slug})
+
+
+    class Meta:
+        db_table = 'events'
+    
+class SponsorOrPartner(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
+    logo = models.FileField(upload_to='sponsor_or_partner/uploads/logo')
+    slug = AutoSlugField(populate_from='name', unique_with='id')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse("ems_event:sponsor_or_partner", kwargs={"slug": self.slug})
+    
+    def get_logo_url(self) -> str:
+        return self.logo.url
+    
+    class Meta:
+        db_table = 'sponsor_or_partners'
+
+
+class Attendee(Models.Model):
+    attendee = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    event = models.ManyToManyField(Event)
+    attending = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        full_name = f'{self.attendee.first_name} {self.attendee.last_name}'
+        return full_name
+    
+    def get_absolute_url(self):
+        return reverse("ems_event:attendee", kwargs={"slug": self.slug})
+
+    class Meta:
+        ordering = ['event']
+        db_table = 'attendees'
