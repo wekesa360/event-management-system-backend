86234fcf5b8e8bff5a22f98939a00787b07cafb4
Michael Wekesa <wekesa360@outlook.com>
2022-11-18 20:53:11 +0300

    update: views

--- ems_event/views.py
+++ ems_event/views.py
@@ -5,26 +5,26 @@ from django.contrib import messages
 from django.core.exceptions import ObjectDoesNotExist
 from django.conf import settings
 from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm
+from django.contrib.auth.forms import PasswordResetForm
+from .forms import PrettyAuthenticationForm, PrettyUserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.tokens import default_token_generator
 from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_encode
+from django.contrib.auth import get_user_model
 from django.utils.encoding import force_bytes
 from django.urls import  reverse
 from .models import (
     Event,
     EventSpeaker,
     Category,
-    CustomUser,
     SponsorOrPartner,
     Attendee
 )
-SignupForm = ''
 
 def signin_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
+        form = PrettyAuthenticationForm(request, data=request.POST)
         if form.is_valid():
             email = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
@@ -32,38 +32,39 @@ def signin_view(request):
             if user is not None:
                 login(request, user)
                 messages.info(request, f'You are logged in!')
-                return redirect('ems_event:dashboard')
+                return redirect('ems:home')
             else:
                 messages.error(request, 'Invalid email or password.')
         else:
             messages.error(request, 'Invalid email or password.')
-    form = AuthenticationForm()
+    form = PrettyAuthenticationForm()
     return render(request, template_name='login.html', context={'form':form})
 
 
-def signup(request):
-    if request.methos == 'POST':
-        form = SignupForm(request.POST)
+def signup_view(request):
+    if request.method == 'POST':
+        form = PrettyUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             messages.success(request, 'Signup successful')
-            return redirect('ems_event:dashboard')
+            return redirect('ems:home')
         messages.error(request, 'Unsuccessful Signup. Invalid information.')
-    form = SignupForm
+        return render(request, template_name='signup.html', context={'form': form})
+    form = PrettyUserCreationForm
     return render(request, template_name='signup.html', context={'form': form})
 
-def logout(request):
+def logout_view(request):
     logout(request)
     messages.info(request, 'You have succcessfully logged out.')
-    return redirect('ems_event:login')
+    return redirect('ems:signin')
 
 def password_reset(request):
     if request.method == 'POST':
         password_reset_form = PasswordResetForm(request.POST)
         if password_reset_form.is_valid():
             data = password_reset_form.cleaned_data['email']
-            associated_users = settings.AUTH_USER_MODEL.objects.filter(email=data)
+            associated_users = get_user_model().objects.filter(email=data)
             if associated_users.exists():
                 for user in associated_users:
                     subject = 'Password Reset Requested'
@@ -92,8 +93,9 @@ def password_reset(request):
 @login_required
 def dasboard_view(request):
     try:
-        user_id = request.user
-        user = settings.AUTH_USER_MODEL.objects.get(id=user_id)
+        user_username = request.user
+        user_model = get_user_model()
+        user = user_model.objects.get(username=user_username)
         events = Event.objects.filter(target_audience=user.user_type)
         # for partners or sponsors, event_speaker, attendees 
         # loop through while conditioning with jinja
@@ -101,11 +103,12 @@ def dasboard_view(request):
         event_speaker = EventSpeaker()
         attendees = Attendee.objects.all()
         if request.method == 'GET':
-            return render(request, 'dashboard.html', context={'events': events, 
+            return render(request, 'index.html', context={'events': events, 
+                                                            'user': user,
                                                             'attendees': attendees,
                                                             'event_speaker':event_speaker,
                                                             'partners_or_sponsors': partners_or_sponsors})
-        return redirect('ems_event:login')
+        return redirect('ems:login')
     except ObjectDoesNotExist:
         print('Error getting Object')
-        return redirect('ems_event: login')
+        return redirect('ems: login')
