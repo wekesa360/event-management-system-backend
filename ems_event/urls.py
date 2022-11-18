d5d87f7c66301b40630d8421e84bfa1753ad45a7
Michael Wekesa <wekesa360@outlook.com>
2022-11-18 20:52:47 +0300

    update: add urls

--- ems_event/urls.py
+++ ems_event/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import dasboard_view
+from .views import dasboard_view, signin_view, logout_view, signup_view
 urlpatterns = [
     path('', dasboard_view, name='home'), # homepage url
-]
+    path('accounts/login/', signin_view, name='signin'), # login page url
+    path('accounts/logout/', logout_view, name='logout'), # logout page url
+    path('accounts/signup', signup_view, name='signup'), # signup page url
+] 
