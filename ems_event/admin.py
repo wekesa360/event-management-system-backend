a2fbe1b37e8795774f971c8af6163bdb689149d7
Michael Wekesa <wekesa360@outlook.com>
2022-11-24 20:59:00 -0800

    fix: clean code

--- ems_event/admin.py
+++ ems_event/admin.py
@@ -3,7 +3,6 @@ from django.contrib.auth.forms import UserChangeForm
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.admin import UserAdmin
 from .models import (
     CustomUser,
     Category,
