e20bfa888ad565d7d0a7cd4f30a13e5cb685c3a1
Michael Wekesa <wekesa360@outlook.com>
2022-11-25 14:53:25 +0300

    update: fix bugs

--- ems_event/functions.py
+++ ems_event/functions.py
@@ -2,7 +2,7 @@ from django.utils import timezone
 from datetime import date
 from django.conf import settings
 from django.core.mail import send_mail as sm
-from django.shortcuts import HttpResponse
+from django.template.loader import render_to_string
 from .models import Event
 from django.contrib.auth import get_user_model
 
@@ -13,8 +13,8 @@ def event_status_check():
             event = Event.objects.get(event_title=event.event_title)
             event.status = 'completed'
             event.save()
-            return HttpResponse(f'Today is: {date.today()} < event {event.event_title} closes on {event.end_date}>')
-    return HttpResponse('Event end_date checker: <No event end_date is today or earlier>')
+            print(f'Today is: {date.today()} <event: {event.event_title} closes on {event.end_date}>')
+    print('Event end_date checker: <No event end_date is today or earlier>')
 
 def get_emails(user_type):
     users = get_user_model().objects.filter(user_type=user_type)
@@ -24,34 +24,45 @@ def get_emails(user_type):
     return emails_dict
 
 def send_signup_email(recipient):
+    msg_html = render_to_string('email/email_signup.html', {'username': recipient.username,
+                                                  'email': recipient.email})
     res = sm(
         subject = 'Event management system account signup',
-        message = 'You have successfully registered an account with event management system!',
+        message = '',
         from_email = settings.EMAIL_HOST_USER,
         recipient_list = [recipient.email],
+        html_message=msg_html,
         fail_silently=False,
     )
-    return HttpResponse(f"Email sent to {res} members")
+    print(f"Email sent to {res} members")
 
 def send_signin_email(recipient):
     subject = 'Event management account signin'
-    message = f'You have successfully signed into account {recipient.username}'
+    message = ''
+    msg_html = render_to_string('email/email_signin.html', {'username':recipient.username,
+                                                            'email': recipient.email})
     res = sm(
         subject=subject,
         message=message,
         from_email = settings.EMAIL_HOST_USER,
         recipient_list = [recipient.email],
-        fail_silently=False)
-    return HttpResponse(f"Email sent to {res} members")
+        html_message=msg_html,
+        fail_silently=False
+    )
+    print(f"Email sent to {res} members")
 
 def send_new_event_email(emails_dict, event_details):
-    for user_email in emails_dict:
-        res  = sm(
-          subject = 'EMS: New Event',
-          message =  f'''Hi {user_email.key()}, \n A new Event has {event_details.event_title} has been created. \n
-          sign in  and check it out''',
-          from_email = settings.EMAIL_HOST_USER,
-          recipient_list = [user_email.value()],
-          fail_silently=True
-        )
-    return HttpResponse(f'Email sent to {res} members')
+    email_list = []
+    for user in emails_dict:
+        email_list.append(emails_dict[user])
+    print(email_list)
+    msg_html = render_to_string('email/email_event.html', {'event': event_details})
+    res  = sm(
+        subject = 'Event Management System: New Event',
+        message =  '',
+        from_email = settings.EMAIL_HOST_USER,
+        recipient_list = email_list,
+        html_message=msg_html,
+        fail_silently=True
+    )
+    print(f'Email sent')
